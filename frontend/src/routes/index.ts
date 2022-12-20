0ef6cb6ae975f946ebbcf16e0fb8d81b475b0f83
Vo Huy Khoa <huykhoa630@gmail.com>
2022-12-20 17:32:25 +0700

    create route frontend

--- frontend/src/routes/index.ts
+++ frontend/src/routes/index.ts
@@ -1,1 +1,27 @@
+import config from "../configs";
+import Home from "../pages/Home/home";
 
+const configRoutes = config.routes;
+
+const publicRoutes = [
+  {
+    path: configRoutes.home,
+    component: Home,
+    layout: null,
+  },
+];
+
+const privateRoutes = [
+  {
+    path: configRoutes.message,
+    component: Home,
+    layout: null,
+  },
+  {
+    path: configRoutes.upload,
+    component: Home,
+    layout: null,
+  },
+];
+
+export { publicRoutes, privateRoutes };
