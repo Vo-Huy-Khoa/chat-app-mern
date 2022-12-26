0f80170405019f7e6b3e991526b7332d8eb8b03c
Khoa VH <huykhoa630@gmail.com>
2022-12-26 20:52:28 +0700

    create sidebar frontend

--- frontend/src/components/common/layouts/sidebar/index.tsx
+++ frontend/src/components/common/layouts/sidebar/index.tsx
@@ -1,6 +1,8 @@
 import styles from "./sidebar.module.scss";
 import classNames from "classnames/bind";
 import Image from "../../../ui/Image";
+import { NotificationIcon } from "../../../ui/Icon";
+import {AccountItem, AccountMessage} from "../../../ui/AccountItem";
 
 const cx = classNames.bind(styles);
 
@@ -23,26 +25,51 @@ const message = {
   avatar:
     "https://cdn.24h.com.vn/upload/3-2021/images/2021-09-21/anh-2-1632216610-256-width650height867.jpg",
   name: "Anh Thy",
-  username: "khoavh",
+  react: "haha",
   message: "chat bot",
   time: "9:50",
 };
 
 const Sidebar = () => {
   return (
-    <div className={cx("container")}>
+    <div className={cx("wrapper")}>
       <div className={cx("header")}>
-        <Image />
-        <h1 className={cx("header_heading")}></h1>
-        <div className={cx("header_notification")}></div>
+        <div className={cx("header_heading")}>
+          <Image
+            width="50px"
+            height="50px"
+            src="https://bedental.vn/wp-content/uploads/2022/11/gai-xinh-nguc-bu-7.jpg"
+          />
+          <h1 className={cx("header_title")}>Vo Huy Khoa</h1>
+        </div>
+        <div className={cx("header_notification")}>
+          <NotificationIcon width="50px" height="50px" />
+          <span>1</span>
+        </div>
       </div>
 
       <div className={cx("status")}>
-        <div className={cx("status_search")}>
+        {/* <div className={cx("status_search")}>
           <input type="text" />
+        </div> */}
+        <div className={cx("status_doing")}>
+          <span className={cx("status--title", "text-white")}>Favorites</span>
+          <div className={cx("status_content")}>
+            <AccountItem data={status} />
+            <AccountItem data={status} />
+            <AccountItem data={status} />
+            <AccountItem data={status} />
+          </div>
         </div>
       </div>
-      <div className={cx("message")}></div>
+      <div className={cx("message")}>
+        <AccountMessage data= {message} />
+        <AccountMessage data= {message} />
+        <AccountMessage data= {message} />
+        <AccountMessage data= {message} />
+        <AccountMessage data= {message} />
+        <AccountMessage data= {message} />
+      </div>
     </div>
   );
 };
--- frontend/src/components/ui/Image/index.tsx
+++ frontend/src/components/ui/Image/index.tsx
@@ -3,7 +3,7 @@ import classNames from "classnames/bind";
 const cx = classNames.bind(styles);
 
 const Image = ({ src, width = "32px", height = "32px", type='' }: any) => {
-  return <img className={cx(type === 'notification' && 'notification')} src={src} width={width} height={height} alt="" />;
+  return <img className={cx( 'image')} src={src} width={width} height={height} alt="" />;
 };
 
 export default Image;
