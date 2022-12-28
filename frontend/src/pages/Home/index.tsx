eef746e9cbd7bca6012e69e59211505fd3d26384
Vo Huy Khoa <huykhoa630@gmail.com>
2022-12-28 17:38:21 +0700

    update layout

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -12,30 +12,17 @@ const profile = {
 };
 const Home = () => {
   return (
-<<<<<<< HEAD
-    <div className={cx("container")}>
+    <div className={cx("wrapper")}>
       <div className={cx("header", "fixed")}>
-        <div className={cx("header_content")}>
-          <Image src={profile.avatar} width="50px" height="50px" />
-          <div className={cx("header_text")}>
+        <div className={cx("content")}>
+          <Image src={profile.avatar} width="60px" height="60px" />
+          <div className={cx("text")}>
             <span>{profile.name}</span>
             <p>{profile.username}</p>
           </div>
         </div>
       </div>
 
-=======
-    <div className={cx("wrapper")}>
-      <div className={cx("header", "fixed")}>
-   <div className={cx('content')}>
-          <Image src={profile.avatar} width="60px" height="60px" />
-          <div className={cx("text")}>
-            <span>{profile.name}</span>
-            <p>{profile.username}</p>
-          </div>
-   </div>
-      </div>
->>>>>>> 3c0d1fb545ed6f8df35f6a5ea90c379daf68aa37
       <div className={cx("content")}></div>
 
       <div className={cx("message", "fixed")}></div>
