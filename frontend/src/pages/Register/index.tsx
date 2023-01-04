f0bf8ea198285dbf9d1226dcbd18ff84a9f481da
Vo Huy Khoa <huykhoa630@gmail.com>
2023-01-04 17:34:02 +0700

    create handle register

--- frontend/src/pages/Register/index.tsx
+++ frontend/src/pages/Register/index.tsx
@@ -1,22 +1,30 @@
 import styles from "./register.module.scss";
 import classNames from "classnames/bind";
 import { NavLink } from "react-router-dom";
+import { useRef } from "react";
+
+
 const cx = classNames.bind(styles);
 
 const Register = () => {
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const fullNameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null)
+
   return (
     <div className={cx("container")}>
       <div className={cx("register")}>
         <div className={cx("heading")}>
           <h1>Register</h1>
         </div>
-        <form className={cx('form_register')} action="" method="post">
+        <form className={cx("form_register")}>
           <div className={cx("form__item")}>
             <label htmlFor="">Full Name</label>
             <input
+            ref={fullNameRef}
               type="text"
               id="fullname"
-              name="fullname"
               placeholder="Enter your name"
             />
           </div>
@@ -25,18 +33,12 @@ const Register = () => {
             <input
               type="text"
               id="username"
-              name="username"
               placeholder="Enter your user name"
             />
           </div>
           <div className={cx("form__item")}>
             <label htmlFor="">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Enter your email"
-            />
+            <input type="email" id="email" placeholder="Enter your email" />
           </div>
           <div className={cx("form__item")}>
             <label htmlFor="">Password</label>
