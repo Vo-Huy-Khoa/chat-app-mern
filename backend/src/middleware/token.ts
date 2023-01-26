54c70f444c96714f7376867fdf0329a262dceef8
Khoa VH <huykhoa630@gmail.com>
2023-01-26 14:59:18 +0700

    update token

--- backend/src/middleware/token.ts
+++ backend/src/middleware/token.ts
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { Request, Response, NextFunction, response } from "express";
+import { Request, Response, NextFunction } from "express";
 
 const createToken = (user: any) => {
   const JWT_SECRET = process.env.JWT_SECRET || "";
