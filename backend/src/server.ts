6a5769c69bbb42f63194ab712cfa28a990f732c2
Khoa VH <78124749+Vo-Huy-Khoa@users.noreply.github.com>
2023-02-22 09:46:38 +0700

    fix CORS all request

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -22,6 +22,15 @@ const options: cors.CorsOptions = {
 };
 app.use(cors(options));
 
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+});
+
 dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
