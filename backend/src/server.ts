fd891e95f89a98e348cfdb42af6e6b3cf9cc7632
Khoa VH <78124749+Vo-Huy-Khoa@users.noreply.github.com>
2023-02-22 10:48:06 +0700

    fix cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -20,22 +20,9 @@ const PORT = process.env.PORT || "3001";
 //   origin: allowedOrigins,
 //   credentials: true,
 // };
-// app.use(cors(options));
+app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", "true");
 
-  next();
-});
 
 
 dotenv.config();
