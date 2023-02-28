77483e714944dbcbc86ae74b245c7d08052d3dc1
KhoaVH <huykhoa630@gmail.com>
2023-02-28 22:03:39 +0700

    update navigate route auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
+import { useContext } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Auth, Dashboard } from "./layouts";
+import { AuthContext } from "./providers";
+
 function App() {
+  const { auth } = useContext(AuthContext);
   return (
     <Routes>
       <Route path="/dashboard/*" element={<Dashboard />} />
       <Route path="/auth/*" element={<Auth />} />
-      <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
+      <Route
+        path="/auth/*"
+        element={auth && <Navigate to="/dashboard/home" replace />}
+      />
+      <Route
+        path="*"
+        element={!auth && <Navigate to="/auth/sign-in" replace />}
+      />
     </Routes>
   );
 }
--- frontend/src/pages/auth/sign-in.tsx
+++ frontend/src/pages/auth/sign-in.tsx
@@ -1,12 +1,14 @@
 import classNames from "classnames/bind";
 import { NavLink, useNavigate } from "react-router-dom";
-import { useRef } from "react";
+import { useContext, useRef } from "react";
 import { FacebookIcon, GoogleIcon, GithubIcon } from "../../components";
 import styles from "../../assets/scss/login.module.scss";
 import { handleLogin } from "../../services/auth";
+import { AuthContext } from "../../providers";
 
 const cx = classNames.bind(styles);
 const Login = () => {
+  const { handleAuth } = useContext(AuthContext);
   const navigate = useNavigate();
   const userRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
@@ -20,7 +22,8 @@ const Login = () => {
     try {
       handleLogin(body)
         .then(() => {
-          navigate("/");
+          navigate("/dashboard/home");
+          handleAuth(true);
         })
         .catch((error) => {
           alert("Login Fail!");
