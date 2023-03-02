b76969ebb03580db95aebd6874a3fca3d30bbd50
KhoaVH <huykhoa630@gmail.com>
2023-03-02 17:04:26 +0700

    refuctor folder backend

--- backend/src/controllers/messageController.ts
+++ backend/src/controllers/messageController.ts
@@ -1,62 +1,44 @@
-import MessageModel from "../models/Message";
 import { Request, Response } from "express";
+import MessageModel from "../models/Message";
 import UserModel from "../models/User";
 
 class MessageController {
   async getListMessage(req: Request, res: Response) {
+    const { senderID, receiverID } = req.body;
     try {
-      await MessageModel.find({
-        $or: [
-          { senderID: req.body.senderID },
-          { receiverID: req.body.receiverID },
-        ],
+      const listMessage = await MessageModel.find({
+        $or: [{ senderID }, { receiverID }],
       })
         .populate("senderID")
-        .populate("receiverID")
-        .then((listMessage) => {
-          res.status(200).json(listMessage);
-        })
-        .catch((error) => {
-          res.status(400).json(error);
-        });
+        .populate("receiverID");
+      res.status(200).json(listMessage);
     } catch (error) {
-      console.log(error);
+      res.status(400).json(error);
     }
   }
 
   async createMessage(data: any, io: any, socket: any, listSocketID: any) {
-    const createMessage = new MessageModel({
-      senderID: data.senderID,
-      receiverID: data.receiverID,
-      message: data.message,
-    });
+    const { senderID, receiverID, message } = data;
+    const createMessage = new MessageModel({ senderID, receiverID, message });
 
     try {
       await createMessage.save();
-      const messageSender = await MessageModel.find({
-        senderID: data.senderID,
-        receiverID: data.receiverID,
-      })
-        .populate("senderID")
-        .populate("receiverID");
-      const messageReceiver = await MessageModel.find({
-        senderID: data.receiverID,
-        receiverID: data.senderID,
-      })
-        .populate("senderID")
-        .populate("receiverID");
+      const [messageSender, messageReceiver, user] = await Promise.all([
+        MessageModel.find({ senderID, receiverID })
+          .populate("senderID")
+          .populate("receiverID"),
+        MessageModel.find({ senderID: receiverID, receiverID: senderID })
+          .populate("senderID")
+          .populate("receiverID"),
+        UserModel.findById(receiverID),
+      ]);
 
-      const message = [...messageSender, ...messageReceiver];
+      const messages = [...messageSender, ...messageReceiver];
 
-      const user = await UserModel.findById({ _id: data.receiverID });
-
-      // Send the message to the other client
-      if (listSocketID.includes(user?.socketID)) {
-        io.to(`${user?.socketID}`).emit("message", message);
+      if (user?.socketID && listSocketID.includes(user.socketID)) {
+        io.to(user.socketID).emit("message", messages);
       }
-      socket.emit("message", message);
-
-      // res.status(201).json(message);
+      socket.emit("message", messages);
     } catch (error) {
       // res.status(400).json(error);
     }
@@ -73,7 +55,14 @@ class MessageController {
           _id: false,
         }
       );
-      res.status(201).json(message);
+
+      if (message.length === 0) {
+        return res.status(404).json({
+          error: "No message found for the given sender and receiver IDs",
+        });
+      }
+
+      res.status(200).json(message);
     } catch (error) {
       res.status(400).json(error);
     }
