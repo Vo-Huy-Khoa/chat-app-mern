221f05ab8649f809df31d8de0c5f987f0bfcb804
KhoaVH <huykhoa630@gmail.com>
2023-04-21 11:55:53 +0700

    update style max-width image

--- frontend/src/components/Image.tsx
+++ frontend/src/components/Image.tsx
@@ -5,7 +5,7 @@ export const Image = ({ src, width = "32px", height = "32px" }: any) => {
       width={width}
       height={height}
       alt=""
-      className="rounded-full w-24 h-24 object-cover cursor-pointer"
+      className="rounded-full w-24 h-24 object-cover cursor-pointer max-w-none"
     />
   );
 };
