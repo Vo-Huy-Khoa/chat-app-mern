fe00c02348633aef20b3a639d3d8a682b4b5007d
KhoaVH <huykhoa630@gmail.com>
2023-04-25 09:21:48 +0700

    update allow cors

--- frontend/src/services/instance.ts
+++ frontend/src/services/instance.ts
@@ -24,6 +24,10 @@ const instanceAxios = axios.create({
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+    "Access-Control-Allow-Headers":
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
   },
 });
 
