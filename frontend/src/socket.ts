432f9200c3e8124909afc27b8fe54d5c0f777371
KhoaVH <78124749+Vo-Huy-Khoa@users.noreply.github.com>
2023-06-05 09:13:33 +0700

    update url socket

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,7 +1,6 @@
 import io from "socket.io-client";
 
-// const url = "http://localhost:3001";
-const url = "https://chat-backend-7jgs.onrender.com";
+const url = "https://chat-app-u9j0.onrender.com";
 const socket = io(url, {
   transports: ["websocket"],
 });
